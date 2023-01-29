92ff839d42efec2bd8130df4f2c49cc7013f91a3
Kevin Paul <krpaul2004@gmail.com>
2023-01-29 11:39:15 -0500

    updates

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from data import ranked, capitals, find_coords_of_cap, create_radius
+from charity.grab import grab_charities
+from time import time
+
+import geopy.distance
 
 app = Flask(__name__)
 
@@ -29,3 +33,19 @@ def heat():
             data2.extend(a)
 
     return jsonify(data2)
+
+last_req = 0;
+@app.route("/get-charities")
+def chars():
+    if not time() - last_req > 5:
+        return jsonify({})
+
+    min_d = 99999999
+    cap = None
+    for c in capitals:
+        dist = geopy.distance.geodesic((request.args["lat"], request.args["lng"]), (float(c["CapitalLatitude"]), float(c["CapitalLongitude"]))).km
+        if dist < min_d:
+            min_d = dist
+            cap = c
+
+    return jsonify(grab_charities(cap["CountryName"]))
